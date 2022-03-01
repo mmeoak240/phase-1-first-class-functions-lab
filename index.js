515a0eb3af26bf807a6cd69334ae7a802269fc4d
Michael Meoak <mmeoak240@gmail.com>
2022-02-28 18:07:44 -0700

    commit tree 3887a45ce847de0fa80e46f7711a9311bb8d5733

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(array) {
+  return [...array.slice(0, 2)]
+  };
+
+const returnLastTwoDrivers = function(array) { return [...array.slice(2)]};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(fare) {
+   return function() {
+     return fare * 5
+  }
+};
+
+const fareMultiplier = createFareMultiplier(2);
+const fareQuintupler = createFareMultiplier(5);
+
+const fareDoubler = function(int) {
+  return (int * 2);
+}
+
+fareDoubler(10);
+
+function fareTripler(fare) {
+  return (fare * 3)
+   }
+
+fareTripler(12);
+
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+  return returnFirstTwoDrivers(arrayOfDrivers)
+  }
